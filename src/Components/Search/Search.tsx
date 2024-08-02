7f5f9ba7e31940a165c0032a09e599b948206aeb
Johans Eris <johans.justs.eris@gmail.com>
2024-08-02 14:48:49 +0300

    Add a button event

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import { useState } from 'react';
 
-interface Props {}
+interface Props { }
 
-const Search: React.FC<Props> = (props: Props) : JSX.Element => {
+const Search: React.FC<Props> = (props: Props): JSX.Element => {
     const [search, setSearch] = useState<string>("");
-    const onclick = (e: any) => {
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
+        console.log(e);
+    }
+
+    const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         console.log(e);
     }
 
     return (
         <div>
             <p>{search}</p>
-            <input 
+            <input
                 title='input search'
-                value={search} onChange={
-                    (e) => onclick(e)
+                value={search}
+                onChange={
+                    (e) => handleChange(e)
                 }
             >
             </input>
+            <button
+                type='button'
+                onClick={
+                    (e) => onClick(e)
+                }
+            >
+                Button
+            </button>
         </div>
     );
 }
